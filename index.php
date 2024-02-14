f3e988dbdcabb2ebbb55a39c8b8a2090d574ccc8
Fernanda dos Santos <159660295+fe-tabs@users.noreply.github.com>
2024-02-13 23:36:16 -0300

    :sparkles: feat: insert products

--- index.php
+++ index.php
@@ -24,6 +24,51 @@
   </head>
 
   <body>
+    <form action="#" method="POST">
+      <label for="description">Descrição</label>
+      <input id="description" name="description" type="text"/>
+      <br/>
+      <label for="ean_code">Código de Barras</label>
+      <input id="ean_code" name="ean_code" type="text"/>
+      <br/>
+      <label for="retail_price">Preço de Varejo</label>
+      <input 
+        id="retail_price" 
+        name="retail_price" 
+        type="number"
+        step=".01"
+        min=".01"
+        max="99999.99"
+        value=".01"
+      />
+      <br/>
+      <label for="wholesale_price">Preço de Atacado</label>
+      <input 
+        id="wholesale_price" 
+        name="wholesale_price" 
+        type="number"
+        step=".01"
+        min=".01"
+        max="99999.99"
+        value=".01"
+      />
+      <br/>
+      <label for="details">Detalhes</label>
+      <textarea
+        id="details"
+        name="details"
+        maxlength="500"
+      >
+      </textarea>
+      <br/>
+      <label for="section">Seção</label>
+      <input id="section" name="section" type="text"/>
+
+      <input type="submit"/>
+    </form>
+
+    <hr/>
+
     <table>
       <thead>
         <th>Descrição</th>
@@ -48,3 +93,16 @@
     </table>
   </body>
 </html>
+
+<?php
+
+  if (isset($_POST['description']) && !empty($_POST['description'])) {
+    $fields = implode("`, `", array_keys($_POST));
+    $values = implode("', '", $_POST);
+
+    $query = "INSERT INTO `products` (`$fields`) VALUES ('$values')";
+
+    $reponse = mysqli_query($conn, $query) or die(mysqli_error($conn));
+  }
+
+?>
