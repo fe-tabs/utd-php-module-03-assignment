b93debea680372c20956fd5da909f036b5a91b47
Fernanda dos Santos <159660295+fe-tabs@users.noreply.github.com>
2024-02-14 00:23:11 -0300

    :sparkles: feat: search products by ean code

--- index.php
+++ index.php
@@ -2,14 +2,26 @@
 
   require_once 'connection.php';
 
-  $query = "SELECT * FROM `products`;";
+  if (isset($_POST['ean_code']) && !empty($_POST['ean_code'])) {
+    $query = "SELECT * FROM `products` WHERE `ean_code` = '".
+      $_POST['ean_code']
+    ."';";
 
-  $response = mysqli_query($conn, $query) or die(mysqli_error($conn));
-
-  $data = array();
-
-  while ($row = mysqli_fetch_assoc($response)) {
-    $data[] = $row;
+    $response = mysqli_query($conn, $query) or die(mysqli_error($conn));
+  
+    while ($row = mysqli_fetch_assoc($response)) {
+      $data[] = $row;
+    }
+  } else {
+    $query = "SELECT * FROM `products`;";
+  
+    $response = mysqli_query($conn, $query) or die(mysqli_error($conn));
+  
+    $data = array();
+  
+    while ($row = mysqli_fetch_assoc($response)) {
+      $data[] = $row;
+    }
   }
 
 ?>
@@ -25,44 +37,8 @@
 
   <body>
     <form action="#" method="POST">
-      <label for="description">Descrição</label>
-      <input id="description" name="description" type="text"/>
-      <br/>
       <label for="ean_code">Código de Barras</label>
       <input id="ean_code" name="ean_code" type="text"/>
-      <br/>
-      <label for="retail_price">Preço de Varejo</label>
-      <input 
-        id="retail_price" 
-        name="retail_price" 
-        type="number"
-        step=".01"
-        min=".01"
-        max="99999.99"
-        value=".01"
-      />
-      <br/>
-      <label for="wholesale_price">Preço de Atacado</label>
-      <input 
-        id="wholesale_price" 
-        name="wholesale_price" 
-        type="number"
-        step=".01"
-        min=".01"
-        max="99999.99"
-        value=".01"
-      />
-      <br/>
-      <label for="details">Detalhes</label>
-      <textarea
-        id="details"
-        name="details"
-        maxlength="500"
-      >
-      </textarea>
-      <br/>
-      <label for="section">Seção</label>
-      <input id="section" name="section" type="text"/>
 
       <input type="submit"/>
     </form>
@@ -93,16 +69,3 @@
     </table>
   </body>
 </html>
-
-<?php
-
-  if (isset($_POST['description']) && !empty($_POST['description'])) {
-    $fields = implode("`, `", array_keys($_POST));
-    $values = implode("', '", $_POST);
-
-    $query = "INSERT INTO `products` (`$fields`) VALUES ('$values')";
-
-    $reponse = mysqli_query($conn, $query) or die(mysqli_error($conn));
-  }
-
-?>
