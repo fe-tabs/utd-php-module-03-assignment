a74cea6e03ffe48dde3bc80209d08fadd0c5182e
Fernanda dos Santos <159660295+fe-tabs@users.noreply.github.com>
2024-02-18 19:45:14 -0300

    :sparkles: feat: tables pagination

--- components/table.php
+++ components/table.php
@@ -1,16 +1,27 @@
-<table>
-  <thead>
-    <?php foreach ($tableColumns as $column) : ?>
-      <th><?= $column; ?></th>
-    <?php endforeach; ?>
-  </thead>
-  <tbody>
-    <?php foreach ($tableData as $item) : ?>
-      <tr>
-        <?php foreach ($item as $itemData) : ?>
-          <td><?= $itemData; ?></td>
+<div class="card">
+  <div class="card-body overflow-auto">
+    <table class="table table-sm table-responsive align-middle">
+      <thead class="align-middle text-nowrap">
+        <?php foreach ($tableColumns as $column) : ?>
+          <th style="min-width: 160px;"><?= $column; ?></th>
         <?php endforeach; ?>
-      </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
+      </thead>
+      <tbody>
+        <?php foreach ($tableData as $item) : ?>
+          <tr>
+            <?php foreach ($item as $itemData) : ?>
+              <td><?= $itemData; ?></td>
+            <?php endforeach; ?>
+          </tr>
+        <?php endforeach; ?>
+      </tbody>
+    </table>
+  </div>
+  <div class="card-footer">
+    <nav>
+      <ul class="pagination">
+
+      </ul>
+    </nav>
+  </div>
+</div>
--- index.php
+++ index.php
@@ -21,8 +21,8 @@
 
     <title>Busca Preço</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
     <link href="css/styles.css" rel="stylesheet" />
+    <link href="css/pagination.css" rel="stylesheet" />
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
   </head>
 
@@ -44,9 +44,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
     crossorigin="anonymous"></script>
     <script src="js/scripts.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js"
-    crossorigin="anonymous"></script>
-    <script src="js/datatables-simple-demo.js"></script>
-
+    <script src="js/pagination.js"></script>
   </body>
 </html>
